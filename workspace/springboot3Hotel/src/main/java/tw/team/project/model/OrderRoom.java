2d61ece4b86392897f00d93843c7ed7be4d2be3f
DESKTOP-MRQ45D3\User <kawhi060941@gmail.com>
2024-05-14 14:40:00 +0800

    修改錯誤

--- workspace/springboot3Hotel/src/main/java/tw/team/project/model/OrderRoom.java
+++ workspace/springboot3Hotel/src/main/java/tw/team/project/model/OrderRoom.java
@@ -34,16 +34,6 @@ public class OrderRoom {
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date orderDate;
 	
-	public String getGender() {
-		return gender;
-	}
-
-	public void setGender(String gender) {
-		this.gender = gender;
-	}
-
-	private String gender;
-	
 	@Column(name = "order_person_name", nullable = false)
 	private String customerName;
 	
