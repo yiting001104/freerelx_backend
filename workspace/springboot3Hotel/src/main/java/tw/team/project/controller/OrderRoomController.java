8a6dfd7fbd100508ebab4abb50006ca9addef310
jaras0209 <boss57877@gmail.com>
2024-05-16 22:58:50 +0800

    add @CrossOrigin

--- workspace/springboot3Hotel/src/main/java/tw/team/project/controller/OrderRoomController.java
+++ workspace/springboot3Hotel/src/main/java/tw/team/project/controller/OrderRoomController.java
@@ -5,6 +5,7 @@ import org.springframework.boot.configurationprocessor.json.JSONArray;
 import org.springframework.boot.configurationprocessor.json.JSONException;
 import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import tw.team.project.util.JsonContainer;
 
 @RestController
 @RequestMapping("/hotel")
+@CrossOrigin
 public class OrderRoomController {
 
 	@Autowired
--- workspace/springboot3Hotel/src/main/java/tw/team/project/controller/OrderRoomDetailController.java
+++ workspace/springboot3Hotel/src/main/java/tw/team/project/controller/OrderRoomDetailController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,6 +23,7 @@ import tw.team.project.service.OrderRoomService;
 
 @RestController
 @RequestMapping("/hotel")
+@CrossOrigin
 public class OrderRoomDetailController {
 	@Value("${local.serverPort}")
 	private String serverUri;
